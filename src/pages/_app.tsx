6964dc3528e759f1fec1a5de4929e1ebfde869cf
Mayank Pagar <mayankpagar@Mayanks-MacBook-Pro.local>
2023-11-01 19:26:18 +0530

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { initSuperflow } from '@usesuperflow/client'
 
 export default function App({ Component, pageProps }: AppProps) {
+  initSuperflow('SoNazk1CobX8QnhENUBL', { projectId: '7406212340077586' });
   return <Component {...pageProps} />
 }

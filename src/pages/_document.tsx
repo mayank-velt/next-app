ba8f8ea6c85ad37760eba459887175989f46901b
mayank-velt <mayank@velt.dev>
2024-01-18 21:29:38 +0530

    fix: old script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,14 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        {/* <script
+        <script
           id='superflowToolbarScript'
           data-sf-platform='manual'
           async
           src='https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=nfLcY0Nb3xXI35zmi5zh&projectId=5160527548617664'
-        ></script> */}
-		<script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7060838922492968"></script>
-        <NextScript />
+        ></script>
+		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7060838922492968"></script>
+        <NextScript /> */}
       </body>
     </Html>
   )

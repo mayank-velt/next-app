a7681e16ea0bcd395b225e510705b0cc70753988
mayank-velt <mayank@velt.dev>
2024-01-18 21:35:02 +0530

    fix: new new scrip

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,12 +6,7 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        <script
-          id='superflowToolbarScript'
-          data-sf-platform='manual'
-          async
-          src='https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=nfLcY0Nb3xXI35zmi5zh&projectId=5160527548617664'
-        ></script>
+        <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7406212340077586"></script>
 		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7060838922492968"></script>
         <NextScript /> */}
       </body>

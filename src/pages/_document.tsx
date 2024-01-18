351c7d6e50546cb3d4020417cba33186823fae9d
mayank-velt <mayank@velt.dev>
2024-01-18 23:20:38 +0530

    fix: testing new

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,8 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-		<script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=7406212340077586"></script>
+		<script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=5471323604149597"></script>
+		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=7406212340077586"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=8N1OOzEqKz3PCMYsvDbw&projectId=7406212340077586"></script> */}
         {/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7406212340077586"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7060838922492968"></script>

96d7850357e473481a075346033de4b35f1e60b6
mayank-velt <mayank@velt.dev>
2024-01-19 15:53:24 +0530

    fix: renewal logic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,8 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-		<script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=5471323604149597"></script>
+		<script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=qNWpePO9Zdl6Q22ybQfW&projectId=1456973483143298"></script>
+		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=5471323604149597"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=7406212340077586"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=8N1OOzEqKz3PCMYsvDbw&projectId=7406212340077586"></script> */}
         {/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7406212340077586"></script> */}

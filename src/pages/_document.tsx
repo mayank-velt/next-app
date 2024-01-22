b1e541661ac57a2412da097ba4d86e30e2da696a
mayank-velt <mayank@velt.dev>
2024-01-22 22:07:26 +0530

    fix: webflow cancel redirect

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,8 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-		<script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=AVaJyngpVpXIODzXRjfQ&projectId=1087691151546343"></script>
+		<script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=pbV1wsEvB2QoA8mWKz2m&projectId=8757445199272931"></script>
+		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=AVaJyngpVpXIODzXRjfQ&projectId=1087691151546343"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=rIANdRysikby9YNYBwet&projectId=2040344356379785"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=UW5Ah3nQTt4vrKq7F1JB&projectId=7931449580966856"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=ctzbmwduG7glN2pBOkqd&projectId=2170189480629949"></script> */}

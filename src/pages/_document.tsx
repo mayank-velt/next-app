85a72564217e9b3c0332fbf5cb08372abd8e56f6
mayank-velt <mayank@velt.dev>
2024-01-30 19:03:18 +0530

    fix: add domain fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,27 +6,8 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-		<script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=FZod3umPGMNG8tcNrwBA&projectId=8236285902556489"></script>
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=tNXCOoDOekRNpIeIZmMi&projectId=4194427364632485"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=IFtoDHZE4cSEHHzyJoBh&projectId=2758703195227631"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=0fvCU3pi2vRwhU4G1hlr&projectId=7406212340077586"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=yHSybPXZAwSQNeFTmrpO&projectId=8647246964845820"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="webflow-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=VjykxQXty9Vch92QCJD6&projectId=1087691151546343"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=0VyJFNST4yW9agXrkceF&projectId=8757445199272931"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=pbV1wsEvB2QoA8mWKz2m&projectId=8757445199272931"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=AVaJyngpVpXIODzXRjfQ&projectId=1087691151546343"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=rIANdRysikby9YNYBwet&projectId=2040344356379785"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=UW5Ah3nQTt4vrKq7F1JB&projectId=7931449580966856"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=ctzbmwduG7glN2pBOkqd&projectId=2170189480629949"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=04P7uz9c9fkHDSYsvTrx&projectId=8352281113562578"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=bYI1hwwSnVNt7eXXedot&projectId=6758890074802716"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=qNWpePO9Zdl6Q22ybQfW&projectId=1456973483143298"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=5471323604149597"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=9fmMGZUvFxYnWS7RMd6n&projectId=7406212340077586"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=8N1OOzEqKz3PCMYsvDbw&projectId=7406212340077586"></script> */}
-        {/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7406212340077586"></script> */}
-		{/* <script id="superflowToolbarScript" data-sf-platform="other-manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jpxGj4CYeU3c488r2fRC&projectId=7060838922492968"></script>
-        <NextScript /> */}
+		<script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jjreNOLHPRqqp0JNwRDi&projectId=439905073390716"></script>
+		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=FZod3umPGMNG8tcNrwBA&projectId=8236285902556489"></script> */}
       </body>
     </Html>
   )

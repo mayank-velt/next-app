e203c5d6a1c714dee741f9492018e966e6cae115
mayank-velt <mayank@velt.dev>
2024-01-30 19:25:06 +0530

    fix: add domain fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,8 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-		<script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jjreNOLHPRqqp0JNwRDi&projectId=439905073390716"></script>
+		<script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=LzwJzN1Qnsgh3gujwebk&projectId=3462109097308388"></script>
+		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=jjreNOLHPRqqp0JNwRDi&projectId=439905073390716"></script> */}
 		{/* <script id="superflowToolbarScript" data-sf-platform="manual" async src="https://cdn.jsdelivr.net/npm/@usesuperflow/toolbar-staging/superflow.min.js?apiKey=FZod3umPGMNG8tcNrwBA&projectId=8236285902556489"></script> */}
       </body>
     </Html>
